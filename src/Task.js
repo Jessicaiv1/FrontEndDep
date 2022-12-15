f63b16081b02062489128c2921afaf33d97aa224
jessica ivana <jessica.ivana@si.ukdw.ac.id>
2022-12-16 00:22:04 +0700

    commit deploy

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "frontend-fe3114719.netlify.app" // TODO: replace this
+const studentName = "Jessica Ivana Wijayanto" // TODO: replace this
+const studentId = "FE3114719" // TODO: replace this
+
+export { url, studentName, studentId }
